c9ca96c58ccd6bd1ca063b896e7e2fdbc8d5e99d
schmidtde-stout <schmidtde@uwstout.edu>
2022-01-21 11:43:22 -0600

    add a basic health check

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const log = require('loglevel');
 
 module.exports = () => {
   const router = express.Router();
@@ -7,5 +8,15 @@ module.exports = () => {
   router.use(bodyParser.json());
   const usersRoutes = require('./users')();
   router.use('/users', usersRoutes);
+  router.get('/health', (req, res) => {
+    const uptime = process.uptime();
+    const data = {
+      uptime: uptime,
+      message: 'Ok',
+      date: new Date(),
+    };
+    log.info(`${req.method} ${req.originalUrl} success: Online for ${uptime} seconds`);
+    res.status(200).send(data);
+  });
   return router;
 };
